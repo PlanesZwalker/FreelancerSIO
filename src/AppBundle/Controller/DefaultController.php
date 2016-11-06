406d35f48568ad2ddb10ac8ca7cf5f4a92661ab8
PlanesZwalker <anne@jadeau.fr>
2016-11-06 21:17:33 +0100

    Pages de Gestions des Utilisateurs du front
    FosUserBundle
    Ajout de Vues twig
    Form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace AppBundle\Controller;
-
+use Silex\Application;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,23 +10,93 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * 
      */
     public function indexAction(Request $request)
     {
+        $test = array('8','test ok');
+        $name= "Admin";
+        
         // replace this example code with whatever you need
-        return $this->render('freelance/index.html.twig');
+        return $this->render('freelance/index.html.twig', array(
+            'test'=>$test,
+            'name'=>$name
+        ));
+    }
+    /**
+     * @Route("/logout", name="logout")
+     * 
+     */
+    public function logoutAction(Request $request)
+    {
+        return $this->render('front/index.html.twig');
+    }
+    
+    /**
+     * @Route("/front", name="front")
+     * 
+     */
+    public function frontAction(Request $request)
+    {
+         return $this->render('/front/index.html.twig');
     }
     
      /**
      * @Route("/test", name="test")
      */
-    public function testAction(Request $request, Application $app)
+    public function testAction(Request $request)
     {
-     $app->get('/hello/{name}', function ($name) use ($app) {
-        return $app['twig']->render('default/index.html.twig', array(
+            $name='test';
+        
+         return $this->render('listfreelancer.html.twig', array(
             'name' => $name,
         )); 
-    });
+   
     }
+     /**
+     * @Route("/listfreelancer", name="listfreelancer")
+     */
+    public function listFreelancerAction(Request $request)
+    {
+            $name='test';
+        
+         return $this->render('listfreelancer.html.twig', array(
+            'name' => $name,
+        )); 
+   
+    }
+     /**
+     * @Route("/listprojet", name="listprojet")
+     */
+    public function listProjetAction(Request $request)
+    {
+            $name='test';
+        
+         return $this->render('listprojet.html.twig', array(
+            'name' => $name,
+        )); 
+   
+    }
+    
+    /**
+     * @Route("/addprojet", name="addprojet")
+     */
+    public function addProjetAction(Request $request)
+    {
+        
+         return $this->render('societe/addprojet.html.twig');
+   
+    }
+    
+    /**
+     * @Route("/editprofil", name="editprofil")
+     */
+    public function editProfilAction(Request $request)
+    {
+        
+         return $this->render('freelance/editprofil.html.twig');
+   
+    }
+
 
 }
