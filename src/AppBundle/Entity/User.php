fc667a3ced3b1b371d09926aba2b3597ad0c038b
PlanesZwalker <anne@jadeau.fr>
2016-11-08 19:41:04 +0100

    ajout de formulaire et images

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -1,11 +1,11 @@
-<?php
+<?php 
 // src/AppBundle/Entity/User.php
 
 namespace AppBundle\Entity;
  
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
- 
+
 /**
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
